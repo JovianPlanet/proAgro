2e88c05973e988bb14c08b3025da4698fd98e099
JovianPlanet <davidpunk@gmail.com>
2023-05-04 19:05:02 -0500

    Added panel detection for lambdas left

--- get_panel.py
+++ get_panel.py
@@ -5,25 +5,34 @@ import numpy as np
 
 from panel_detection.get_blue444_panel import get_blue444_panel
 from panel_detection.get_red650_panel import get_red650_panel
+from panel_detection.get_green531_panel import get_green531_panel
+from panel_detection.get_redEdge705_panel import get_redEdge705_panel
+from panel_detection.get_redEdge740_panel import get_redEdge740_panel
+
+
 from panel_detection.panel_kmeans import get_panel_kmeans
 
 imagePath = '/media/davidjm/Disco_Compartido/david/datasets/ProyectoAgro/AGUACATE23noviembre/BLUE/000'
-imageName = glob.glob(os.path.join(imagePath,'IMG_00**_3.tif'))#[0]#IMG_0038_3
+imageName = glob.glob(os.path.join(imagePath,'IMG_00**_5.tif'))#[0]#IMG_0038_3
 
 panels_1 = []
+panels_2 = []
 panels_3 = []
+panels = [[], [], [], [], []]
 for image in imageName[:]:
     #panels_1.append(get_blue444_panel(image))#(os.path.join(imagePath,'IMG_0032_1.tif'))
-    panels_3.append(get_red650_panel(image))
+    #panels[1].append(get_green531_panel(image))
+    #panels_3.append(get_red650_panel(image))
+    panels[4].append(get_redEdge740_panel(image))
     # if '03_1' in image:
-    #     get_panel_kmeans(image)
-
+    #get_panel_kmeans(image)
 
 
 #print(f'Num panels blue 444 (1) = {len(panels_1)}')
-print(f'Num panels red 650 (3) = {sum(panels_3)}')
-
-
+print(f'Num panels green 531 (2) = {sum(panels[1])}')
+#print(f'Num panels red 650 (3) = {sum(panels_3)}')
+print(f'Num panels Red edge 705 (4) = {sum(panels[3])}')
+print(f'Num panels Red edge 740 (5) = {sum(panels[4])}')
 
 
 
---
+++ panel_detection/get_redEdge740_panel.py
@@ -0,0 +1,79 @@
+import sys
+import cv2
+import os, glob
+import numpy as np
+from .utils import blur, erosion, get_shape, get_intensity
+
+'''
+Funcion que retorna las coordenadas del panel para las imagenes BLUE.
+Sufijo: 5
+Band name: RedEdge-740
+'''
+
+def get_redEdge740_panel(image):
+
+    img = cv2.imread(image)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgray = gray.copy()
+
+    blurred = blur(gray, canny=False)
+
+    #ret, thresh = cv2.threshold(blurred, 120, 255, 0)
+    #thresh = cv2.inRange(blurred, 110, 140)
+    ret, thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    #thresh = cv2.adaptiveThreshold(blurred,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,15,2)
+    eroded = erosion(thresh)
+    # if '29_1' in image:
+    # cv2.imshow(f'Imagen eroded: {image[-15:]}', eroded)
+    # cv2.waitKey(0)
+
+    contours, hierarchy = cv2.findContours(eroded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    i = 0
+    for contour in contours:
+
+        area = cv2.contourArea(contour)
+        perimeter = cv2.arcLength(contour, True)
+        
+        if area > 15000 and len(contour) < 1000:
+
+            n = img.copy()
+
+            cv2.destroyAllWindows()
+
+            epsilon = 0.09 * perimeter
+            approx = cv2.approxPolyDP(contour, epsilon, True)
+
+            intensity = get_intensity(n, approx)
+
+            shape = get_shape(approx)
+
+            # print(f'{image[-15:]}:\n')
+            # print(f'area = {area:.3f}, perimetro = {perimeter:.3f}, Forma = {shape}, intensidad {intensity:.3f}\n')
+            # cv2.drawContours(n, contour, -1, (0, 255 - i*50, 0), 3)
+            # cv2.imshow(f'Imagen {image[-15:]}', n)
+            # cv2.waitKey(0)
+
+
+            if intensity < 165 and intensity > 130 and shape == 'square' and perimeter < 900: # Para BLUE 3 
+
+                print(f'***La imagen {image[-15:]} contiene panel***\n')
+
+                # cv2.drawContours(n, contour, -1, (0, 255 - i*50, 0), 3)
+                # cv2.imshow(f'Imagen {image[-15:]}', n)
+                # cv2.waitKey(0)
+
+                print(f'Area = {area:.3f}, perimetro = {perimeter:.3f}, Num points = {len(contour)} intensidad {intensity:.3f}')
+                print(f'Area panel (aproximado) = {cv2.contourArea(approx)}, Forma = {shape}')
+                print(f'Las coordenadas del panel son: \n\n{approx}\n')
+
+                temp = np.zeros(n.shape, np.uint8)
+                # cv2.drawContours(temp, [approx], -1, (255, 255, 255), -1)
+                # cv2.imshow("Panel segmentado", temp)
+                # cv2.waitKey(0)
+
+                return True #approx, intensity 
+
+    return False #None
+    
